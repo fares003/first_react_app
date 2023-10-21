489327307ede2fac7b22e8fb7c010cbeb995f591
unknown <fzakaria369@gmail.com>
2023-10-21 11:02:09 +0200

    secound commit

---
+++ src/components/WelcomeCL.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class WelcomeCL extends React.Component{
+
+    render(){
+        return (<h1>class component ,{this.props.name}</h1>)
+    } 
+
+}
+export default WelcomeCL;
